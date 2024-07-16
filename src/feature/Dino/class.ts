ffe27a661f76bf3b3300f937f03514dea0277f72
CHOGANGYEOL <rkdduf06@icloud.com>
2024-07-16 18:01:38 +0900

    :sparkles: Dino games

---
+++ src/feature/Dino/class.ts
@@ -0,0 +1,97 @@
+import DinoImage from './assets/dino.png';
+
+interface DinoType {
+	draw: () => void;
+	jump: () => void;
+}
+
+export class Dino implements DinoType {
+	public x: number;
+	public y: number;
+	public width: number;
+	public height: number;
+	private ctx: CanvasRenderingContext2D;
+	private image: HTMLImageElement;
+
+	public isJumping: boolean;
+	private isUp: boolean;
+	private jumpTimer: number;
+
+	constructor(ctx: CanvasRenderingContext2D) {
+		this.x = 10;
+		this.y = 200;
+		this.width = 50;
+		this.height = 50;
+		this.ctx = ctx;
+		this.image = new Image();
+		this.image.src = DinoImage;
+
+		this.isJumping = false;
+		this.isUp = true;
+		this.jumpTimer = 0;
+	}
+	draw() {
+		this.ctx.fillStyle = 'white';
+		this.ctx.fillRect(this.x, this.y, this.width, this.height);
+		this.ctx.drawImage(this.image, this.x, this.y);
+	}
+	private up() {
+		if (this.jumpTimer >= 100) {
+			this.isUp = false;
+		} else {
+			this.y--;
+			this.jumpTimer++;
+		}
+	}
+
+	private down() {
+		if (this.y === 200) {
+			this.isUp = true;
+			this.isJumping = false;
+		} else {
+			this.y++;
+			this.jumpTimer--;
+		}
+	}
+
+	jump() {
+		if (!this.isUp) {
+			this.down();
+		}
+		if (this.isUp) {
+			this.up();
+		}
+	}
+}
+
+interface VillainType {
+	draw: () => void;
+	move: () => void;
+}
+
+export class Villain implements VillainType {
+	public x: number;
+	public y: number;
+	private width: number;
+	private height: number;
+	private ctx: CanvasRenderingContext2D;
+	private image: HTMLImageElement;
+	constructor(ctx: CanvasRenderingContext2D, imageUrl: string) {
+		this.x = 500;
+		this.y = 200;
+		this.width = 50;
+		this.height = 48;
+		this.ctx = ctx;
+		this.image = new Image();
+		this.image.src = imageUrl;
+	}
+	draw() {
+		// this.ctx.fillStyle = 'red';
+		this.ctx.fillRect(this.x, this.y, this.width, this.height);
+		this.ctx.drawImage(this.image, this.x, this.y);
+	}
+	move() {
+		this.x--;
+		this.draw();
+	}
+}
--- src/feature/Dino/hooks.ts
+++ src/feature/Dino/hooks.ts
@@ -1,10 +1,16 @@
 import { useCallback, useEffect, useRef } from 'react';
 
-import { Cactus, Dino } from './const';
+import Cactus from './assets/cactus.png';
+import { Villain, Dino } from './class';
 
 export const useDino = () => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const wrapperRef = useRef<HTMLDivElement>(null);
+	const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+	const dinoRef = useRef<Dino | null>(null);
+	const villainArr = useRef<Villain[]>([]);
+	const frameRef = useRef(0);
+	const animationRef = useRef(0);
 
 	const canvasSizeSetting = useCallback(() => {
 		if (!canvasRef.current || !wrapperRef.current) return;
@@ -20,31 +26,70 @@ export const useDino = () => {
 		};
 	}, []);
 
-	const Playing = () => {
-		const ctx = canvasRef.current?.getContext('2d');
-		if (!ctx) return;
-
-		const dino = new Dino(ctx);
-		dino.draw();
-
-		const cactus = new Cactus(ctx);
-		cactus.draw();
-		frameStart(ctx, dino, cactus);
-	};
-
-	const frameStart = useCallback((ctx: CanvasRenderingContext2D, dino: Dino, cactus: Cactus) => {
-		requestAnimationFrame(() => {
-			frameStart(ctx, dino, cactus);
-		});
-		if (!canvasRef.current) return;
-		ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-		dino.x++;
-		dino.draw();
-	}, []);
-
 	useEffect(() => {
+		initialSetting();
 		Playing();
 	}, []);
+
+	const initialSetting = useCallback(() => {
+		const canvas = canvasRef.current;
+		if (!canvas) return;
+		const ctx = canvas.getContext('2d');
+		if (!ctx) return;
+		ctxRef.current = ctx;
+		dinoRef.current = new Dino(ctx);
+	}, []);
+
+	const Playing = useCallback(() => {
+		const canvas = canvasRef.current;
+		const ctx = ctxRef.current;
+		const dino = dinoRef.current;
+		if (!ctx || !dino || !canvas) return;
+
+		animationRef.current = requestAnimationFrame(Playing);
+		// 시간 초 증가
+		frameRef.current += 1;
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		if (frameRef.current % 200 === 0) {
+			const villain = new Villain(ctx, Cactus);
+			villainArr.current.push(villain);
+		}
+
+		villainArr.current.forEach((cactus, idx, arr) => {
+			// x좌표가 0미만일 경우 제거
+			if (cactus.x < 0) arr.splice(idx, 1);
+			clashCheck(cactus);
+			cactus.move();
+		});
+		if (dino.isJumping) {
+			dino.jump();
+		}
+		dino.draw();
+	}, []);
+
+	const clashCheck = useCallback((villain: Villain): void => {
+		const dino = dinoRef.current;
+		if (!dino) return;
+		const x = villain.x - (dino.x + dino.width);
+		const y = villain.y - (dino.y + dino.height);
+		if (x < 0 && y < 0) {
+			cancelAnimationFrame(animationRef.current);
+		}
+	}, []);
+
+	document.addEventListener('keydown', (e) => {
+		const dino = dinoRef.current;
+		if (!dino) return;
+		if (e.code === 'Space') {
+			dino.isJumping = true;
+		}
+	});
+	document.addEventListener('touchstart', () => {
+		const dino = dinoRef.current;
+		if (!dino) return;
+		dino.isJumping = true;
+	});
 
 	return { canvasRef, wrapperRef };
 };
